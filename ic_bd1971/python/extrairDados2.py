d8af862bbc15b1624517102570b5c21df0561e9d
Kazuya <shinzatokazuya11@gmail.com>
2025-09-29 02:04:38 +0000

    Sun, Sep 28, 2025, 11:04 PM -03:00

--- ic_bd1971/python/extrairDados2.py
+++ ic_bd1971/python/extrairDados2.py
@@ -2,7 +2,7 @@ import sqlite3
 from datetime import datetime
 
 # Conecta com banco de dados
-con_antigo = sqlite3.connect("../ic_bd2003/bd/Dados_brasileirao_2003_2023.db")
+con_antigo = sqlite3.connect("../backup_br_1971.db")
 cur_antigo = con_antigo.cursor()
 
 con_novo = sqlite3.connect("bd/dados_teste_1971.db")
